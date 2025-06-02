73e9f6b83702498c5b9b7b18083d432824904dc6
DanielaOropezaBalcazar <164942966+DanielaOropezaBalcazar@users.noreply.github.com>
2025-06-02 18:48:33 -0400

    Microservicio de Inicio de Sesion

---
+++ inicioSesion/controllers/sesionController.php
@@ -0,0 +1,35 @@
+<!-- services/sesion/controllers/SesionController.php -->
+<?php
+include_once('../model/sesionModel.php');
+session_start();
+
+class SesionController
+{
+  public function iniciarSesion($email, $password, $tipoUsuario)
+  {
+    // Validar credenciales
+    $sesionModel = new SesionModel();
+    $usuario = $sesionModel->validarUsuario($email, $password, $tipoUsuario);
+
+    if ($usuario) {
+      // Usuario autenticado
+      $_SESSION['usuario'] = $usuario;
+      $_SESSION['tipo'] = $tipoUsuario;
+
+      // Mostrar el mensaje según el tipo de usuario
+      if ($tipoUsuario == 'cliente') {
+        // Redirige al archivo mostrarCuentas.php con el ID del cliente
+        header("Location: ../../manejoCuentas/mostrarCliente.php?id=" . $_SESSION['usuario']['id']);
+        exit();
+      } else {
+        // Redirige al archivo mostrarCuentas.php con el ID del organizador
+        header("Location: ../../manejoCuentas/mostrarOrganizador.php?id=" . $_SESSION['usuario']['id']);
+        exit();
+      }
+    } else {
+      // Credenciales incorrectas
+      echo "Credenciales incorrectas.";
+    }
+  }
+}
+?>
