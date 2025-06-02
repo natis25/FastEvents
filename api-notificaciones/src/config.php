1ad6d8b3194718122fadac286301f083f0271057
Mauricio <alan.fernandez@ucb.edu.bo>
2025-06-02 19:13:22 -0400

    subiendo mi cosa

---
+++ api-notificaciones/src/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host' => 'smtp.gmail.com',
+    'username' => 'alan.fernandez@ucb.edu.bo',
+    'password' => 'mjmx hvge ydst funj',
+    'port' => 587,
+    'from' => 'alan.fernandez@ucb.edu.bo',
+    'from_name' => 'FastEvents'
+];
